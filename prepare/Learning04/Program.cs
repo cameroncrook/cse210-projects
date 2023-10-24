4b62bb099432a9f3cb1e44954ded28e08e843f0c
cameroncrook <cameronbcrook@gmail.com>
2023-10-24 13:33:50 -0600

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathAssignment = new MathAssignment("Cameron Crook", "Fractions", "7.2", "8-20");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Cameron Crook", "The Roman Empire", "The Fall of the Roman Empire by Lil Ceasars");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
