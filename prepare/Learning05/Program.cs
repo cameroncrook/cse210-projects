1fd68de9acdef42e191514372eca43a29d4dcfb4
cameroncrook <cameronbcrook@gmail.com>
2023-11-07 13:26:00 -0700

    Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square(5.0, "Blue");
+        shapes.Add(sq);
+
+        Rectangle rc = new Rectangle(5.0, 4.0, "Red");
+        shapes.Add(rc);
+
+        Circle cr = new Circle(5.0, "Green");
+        shapes.Add(cr);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
